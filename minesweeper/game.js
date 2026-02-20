32cd2531ef5776bd7cc5cc723c490624e8b7ffe7
Erhel czc <erhelito@yahoo.fr>
2026-02-20 16:48:00 +0100

    set mines placement

--- minesweeper/game.js
+++ minesweeper/game.js
@@ -10,6 +10,8 @@ function drawGrid() {
             square.classList.add("cell");
             square.setAttribute("row", i);
             square.setAttribute("col", j);
+            square.setAttribute("mine", "false");
+            square.setAttribute("opened", "false");
             grid.appendChild(square);
         }
     }
@@ -18,6 +20,30 @@ function drawGrid() {
     grid.style.gridTemplateRows = `repeat(${nRow}, 1fr)`;
 }
 
+function placeMines() {
+    for (let i = 0; i < nMines; i += 1) {
+        const row = Math.floor(Math.random() * nRow);
+        const col = Math.floor(Math.random() * nCol);
+        const cell = document.querySelector(`.cell[row="${row}"][col="${col}"]`);
+        if (cell.getAttribute("mine") === "true") {
+            i -= 1;
+            continue;
+        }
+        cell.setAttribute("mine", "true");
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     drawGrid();
+    console.log("Grid drawn");
+
+    // place mines after the first click to ensure the first click is never a mine
+    const grid = document.getElementById("board");
+    grid.addEventListener("click", (event) => {
+        if (event.target.classList.contains("cell")) {
+            placeMines();
+        }
+    });
+
+    console.log("Mines placed");
 });
